ebc73275f73b928a1701aa9775f39fc037ed2477
Ross <49296963+diceyecid@users.noreply.github.com>
2022-03-14 15:18:28 -0700

    player square and slot are created

--- app/src/main/java/com/example/thru/GameActivity.kt
+++ app/src/main/java/com/example/thru/GameActivity.kt
@@ -1,12 +1,7 @@
 package com.example.thru
 
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
-import android.graphics.Paint
 import android.os.Bundle
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import com.example.thru.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity()
@@ -20,8 +15,5 @@ class GameActivity : AppCompatActivity()
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Util.hideSystemBars( window )
-
-        binding.score.paint.style = Paint.Style.STROKE
-        binding.score.paint.strokeWidth = 5f
     }
 }
