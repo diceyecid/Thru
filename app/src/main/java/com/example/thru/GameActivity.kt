ac31a2d8d3f54e95e4a92e2f8850228e057b747a
Ross <49296963+diceyecid@users.noreply.github.com>
2022-03-13 21:43:39 -0700

    home page look

--- app/src/main/java/com/example/thru/GameActivity.kt
+++ app/src/main/java/com/example/thru/GameActivity.kt
@@ -10,15 +10,15 @@ import android.view.View
 import android.view.WindowInsets
 import android.widget.LinearLayout
 import android.widget.TextView
-import com.example.thru.databinding.ActivityFullscreenBinding
+import com.example.thru.databinding.ActivityGameBinding
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class FullscreenActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityFullscreenBinding
+    private lateinit var binding: ActivityGameBinding
     private lateinit var fullscreenContent: TextView
     private lateinit var fullscreenContentControls: LinearLayout
     private val hideHandler = Handler()
@@ -72,7 +72,7 @@ class FullscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityFullscreenBinding.inflate(layoutInflater)
+        binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
